deecebabea5d8f6734254c68c57950bf095dd890
CalacalaBoom <zhangyu19990502@163.com>
2021-11-15 19:43:32 +0800

    ArrayList的遍历

---
+++ Traverse_ArrayList/Traverse_ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Traverse_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("朝辞白帝彩云间");
+            list.Add("一花一叶一世界");
+            list.Add("借问酒家何处有");
+            list.Add("Anybody say hello");
+            foreach (string n in list)
+                Console.WriteLine(n);
+            Console.ReadLine();
+        }
+    }
+}
