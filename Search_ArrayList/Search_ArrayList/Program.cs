b65a64ddf2ea445d23353f6a8cc30f4c44f3b94f
CalacalaBoom <zhangyu19990502@163.com>
2021-11-15 19:54:55 +0800

    ArrayList元素的查找

---
+++ Search_ArrayList/Search_ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Search_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArrayList类提供了一个Contain()方法来确定一个元素是否在ArrayList对象中：");
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Console.Write("原数组：");
+            foreach (int n in list)
+            {
+                Console.Write(n+" ");
+            }
+            Console.WriteLine("\n");
+            if (list.Contains(2))
+            {
+                Console.WriteLine("2在该ArrayList对象中");
+            }
+            else
+            {
+                Console.WriteLine("2不在该ArrayList对象中");
+            }
+            Console.ReadLine();
+        }
+    }
+}
