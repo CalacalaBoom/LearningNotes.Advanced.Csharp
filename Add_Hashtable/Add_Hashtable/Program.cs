244c068d27e574a432ccd2cd1702d07e2e7f5dbe
CalacalaBoom <zhangyu19990502@163.com>
2021-11-16 14:19:11 +0800

    哈希表的实例化与向哈希表中添加元素

---
+++ Add_Hashtable/Add_Hashtable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Add_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hashtable用于存储键值对，键与值一一对应。使用Add()方法添加元素");
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("id","stu0001");
+            hashtable.Add("username","CalacalaBoom");
+            hashtable.Add("password","admin");
+            Console.WriteLine(hashtable.Count);
+            Console.ReadLine();
+        }
+    }
+}
