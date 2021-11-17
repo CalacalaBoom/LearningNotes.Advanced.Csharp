f89ac18c6090f43c439a45531a18b5780832a065
CalacalaBoom <zhangyu19990502@163.com>
2021-11-17 16:57:38 +0800

    属性的使用

---
+++ attributes/attributes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attributes
+{
+    class Ex
+    {
+        private string id = "";//私有变量只能在其声明类中使用
+        private string name = "";
+        public string ID//定义私有变量使其可写可读
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name//定义私有变量使其可写可读
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex ex = new Ex();
+            ex.ID = "stu0001";
+            ex.Name = "CalacalaBoom";
+            Console.WriteLine(ex.ID+" "+ex.Name);
+            ex.ID = "stu0002";
+            ex.Name = "SalisaliPia";
+            Console.WriteLine(ex.ID + " " + ex.Name);
+            Console.ReadLine();
+        }
+    }
+}
