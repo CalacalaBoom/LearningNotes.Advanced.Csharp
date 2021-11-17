394c54dac2a0a6cc4be379a5b1f4668fe88f1a06
CalacalaBoom <zhangyu19990502@163.com>
2021-11-17 20:43:32 +0800

    对象的销毁与类的封装

---
+++ DestroyOfObject/DestroyOfObject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestroyOfObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex ex = new Ex();
+            ex = null;
+            GC.Collect();
+            Console.ReadLine();
+        }
+    }
+    class Ex
+    {
+        ~Ex()
+        {
+            Console.WriteLine("Ex的实例化对象已销毁");
+        }
+    }
+}
---
+++ classEncapsulation/classEncapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex ex = new Ex();
+            ex.X = 1;
+            ex.Y = 2;
+            Console.WriteLine(ex.Add());
+            Console.ReadLine();
+        }
+    }
+    class Ex
+    {
+        private int x=0;
+        private int y=0;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Add()
+        {
+            return X + Y;
+        }
+    }
+}
