48c466235e92ff795f21620342c713dd507273cf
CalacalaBoom <zhangyu19990502@163.com>
2021-12-01 15:58:33 +0800

    文件的基本操作

---
+++ System.IO_case/File_BasicOperation/File_BasicOperation/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace File_BasicOperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.find = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnIsExists = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCopyTo = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnMoveTo = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择文件：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(373, 8);
+            this.find.Margin = new System.Windows.Forms.Padding(2);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(52, 27);
+            this.find.TabIndex = 2;
+            this.find.Text = "浏览";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnIsExists);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(8, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(421, 52);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "判断文件是否存在";
+            // 
+            // btnIsExists
+            // 
+            this.btnIsExists.Location = new System.Drawing.Point(365, 18);
+            this.btnIsExists.Margin = new System.Windows.Forms.Padding(2);
+            this.btnIsExists.Name = "btnIsExists";
+            this.btnIsExists.Size = new System.Drawing.Size(52, 27);
+            this.btnIsExists.TabIndex = 1;
+            this.btnIsExists.Text = "判断";
+            this.btnIsExists.UseVisualStyleBackColor = true;
+            this.btnIsExists.Click += new System.EventHandler(this.btnIsExists_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 21);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(349, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCreate);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Location = new System.Drawing.Point(8, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(421, 51);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "创建文件";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(365, 17);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(52, 27);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "创建";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 20);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(349, 21);
+            this.textBox3.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.btnMoveTo);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.btnCopyTo);
+            this.groupBox3.Location = new System.Drawing.Point(8, 153);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(421, 57);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "复制与移动";
+            // 
+            // btnCopyTo
+            // 
+            this.btnCopyTo.Location = new System.Drawing.Point(7, 20);
+            this.btnCopyTo.Name = "btnCopyTo";
+            this.btnCopyTo.Size = new System.Drawing.Size(52, 27);
+            this.btnCopyTo.TabIndex = 0;
+            this.btnCopyTo.Text = "复制至";
+            this.btnCopyTo.UseVisualStyleBackColor = true;
+            this.btnCopyTo.Click += new System.EventHandler(this.btnCopyTo_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(65, 23);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(108, 21);
+            this.textBox4.TabIndex = 1;
+            // 
+            // btnMoveTo
+            // 
+            this.btnMoveTo.Location = new System.Drawing.Point(250, 20);
+            this.btnMoveTo.Name = "btnMoveTo";
+            this.btnMoveTo.Size = new System.Drawing.Size(52, 27);
+            this.btnMoveTo.TabIndex = 2;
+            this.btnMoveTo.Text = "移动至";
+            this.btnMoveTo.UseVisualStyleBackColor = true;
+            this.btnMoveTo.Click += new System.EventHandler(this.btnMoveTo_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(315, 23);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 3;
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Location = new System.Drawing.Point(150, 216);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(135, 28);
+            this.btnGetInfo.TabIndex = 6;
+            this.btnGetInfo.Text = "获取文件基本信息";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 251);
+            this.Controls.Add(this.btnGetInfo);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "文件的基本操作";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIsExists;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnMoveTo;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnCopyTo;
+        private System.Windows.Forms.Button btnGetInfo;
+    }
+}
+
---
+++ System.IO_case/File_BasicOperation/File_BasicOperation/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_BasicOperation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnIsExists_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox2.Text))
+            {
+                MessageBox.Show("该文件已经存在");
+            }
+            else
+            {
+                MessageBox.Show("该文件不存在");
+            }
+        }
+
+        private void btnCreate_Click_1(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox3.Text))
+            {
+                MessageBox.Show("该文件已经存在");
+            }
+            else
+            {
+                File.Create(textBox3.Text);
+                MessageBox.Show(textBox3.Text + "已创建");
+            }
+        }
+
+        private void btnCopyTo_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("请先选择要复制的文件");
+            }
+            else
+            {
+                if (File.Exists(textBox1.Text))
+                {
+                    File.Copy(textBox1.Text, textBox4.Text);
+                    MessageBox.Show("拷贝成功");
+                }
+                else
+                {
+                    MessageBox.Show("该文件不存在");
+                }
+            }
+        }
+
+        private void btnMoveTo_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("请先选择要移动的文件");
+            }
+            else
+            {
+                if (File.Exists(textBox1.Text))
+                {
+                    File.Move(textBox1.Text, textBox5.Text);
+                    MessageBox.Show("移动成功");
+                }
+                else
+                {
+                    MessageBox.Show("该文件不存在");
+                }
+            }
+        }
+
+        private void btnGetInfo_Click(object sender, EventArgs e)
+        {
+            FileInfo finfo = new FileInfo(textBox1.Text);
+            string strCTime = finfo.CreationTime.ToString();
+            string strLATime = finfo.LastAccessTime.ToString();
+            string strLWTime = finfo.LastWriteTime.ToString();
+            string strName = finfo.Name;
+            string strFName = finfo.FullName;
+            string strDName = finfo.DirectoryName;
+            string strISRead = finfo.IsReadOnly.ToString();
+            long strLength = finfo.Length;
+            MessageBox.Show("----------文件信息----------" +
+                "\n创建时间：" + strCTime +
+                "\n上次访问该文件时间：" + strLATime +
+                "\n上次写入该文件时间：" + strLWTime +
+                "\n文件名：" + strName +
+                "\n文件的完整目录：" + strFName +
+                "\n文件的完整路径：" + strDName +
+                "\n文件是否只读：" + strISRead +
+                "\n文件的长度：" + strLength);
+        }
+    }
+}
