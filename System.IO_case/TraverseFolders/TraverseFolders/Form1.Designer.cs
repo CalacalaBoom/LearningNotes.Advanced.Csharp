1600a2052551794ce2e7b0d4cb7411b15a88c575
CalacalaBoom <zhangyu19990502@163.com>
2021-12-01 19:51:22 +0800

    遍历文件夹

---
+++ System.IO_case/TraverseFolders/TraverseFolders/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TraverseFolders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择文件夹：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(424, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(546, 21);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 35);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "浏览";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 71);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(628, 302);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件名";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "路径";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "大小";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "创建时间";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 385);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "遍历文件夹";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ System.IO_case/TraverseFolders/TraverseFolders/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TraverseFolders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+                FileSystemInfo[] fsinfos = directoryInfo.GetFileSystemInfos();
+                foreach (FileSystemInfo f in fsinfos)
+                {
+                    if (f is DirectoryInfo)
+                    {
+                        DirectoryInfo df = new DirectoryInfo(f.FullName);
+                        listView1.Items.Add(df.Name);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(df.FullName);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add("null");
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(df.CreationTime.ToShortDateString());
+                    }
+                    else
+                    {
+                        FileInfo ff = new FileInfo(f.FullName);
+                        listView1.Items.Add(ff.Name);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(ff.FullName);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add((ff.Length/8/1024).ToString()+"KB");
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(ff.CreationTime.ToShortDateString());
+                    }
+                }
+            }
+        }
+    }
+}
