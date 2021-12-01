d2360952535257478b1ad3bd03dbdbddf0148fe5
CalacalaBoom <zhangyu19990502@163.com>
2021-12-01 08:35:22 +0800

    异常捕获语句

---
+++ ExceptionHandlingStatement/ExceptionHandlingStatement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingStatement
+{
+    class Program
+    {
+        static int division(string s1, string s2)
+        {
+            try
+            {
+                if (int.Parse(s2) == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                int num = int.Parse(s1) / int.Parse(s2);
+                return num;
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("尝试用0作为分母");
+                Console.WriteLine(de.Message);
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("请输入分子：");
+                string s1 = Console.ReadLine();
+                Console.Write("\n请输入分母：");
+                string s2 = Console.ReadLine();
+                Console.Write("\n"+s1+"/"+s2+"="+division(s1,s2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("捕获异常：" + ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
