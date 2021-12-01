331f0b5f865ab027a957ee964985cdbf40890d1a
CalacalaBoom <zhangyu19990502@163.com>
2021-12-01 08:51:48 +0800

    抽象类与抽象方法

---
+++ abstract_ClassAndMethod/abstract_ClassAndMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_ClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveClass driveClass = new DriveClass();
+            myClass myClass = driveClass;
+            myClass.id = "AQ12138";
+            myClass.Name = "CalacalaBoom";
+            myClass.showInfo();
+            Console.ReadLine();
+        }
+    }
+    public abstract class myClass
+    {
+        private string ID;
+        private string name;
+        public string id
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public abstract void showInfo();
+    }
+    public class DriveClass : myClass
+    {
+        public override void showInfo()
+        {
+            Console.Write(id + " " + Name);
+        }
+    }
+}
