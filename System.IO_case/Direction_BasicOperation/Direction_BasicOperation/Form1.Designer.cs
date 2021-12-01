1f52f506dcd9bb7a764f5bce71128db6e16c482f
CalacalaBoom <zhangyu19990502@163.com>
2021-12-01 17:12:00 +0800

    文件夹基本操作

---
+++ System.IO_case/Direction_BasicOperation/Direction_BasicOperation/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Direction_BasicOperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnIsExsis = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnMoveTo = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnIsExsis);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(569, 63);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "判断文件夹是否存在";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnIsExsis
+            // 
+            this.btnIsExsis.Location = new System.Drawing.Point(453, 20);
+            this.btnIsExsis.Name = "btnIsExsis";
+            this.btnIsExsis.Size = new System.Drawing.Size(84, 31);
+            this.btnIsExsis.TabIndex = 1;
+            this.btnIsExsis.Text = "判断";
+            this.btnIsExsis.UseVisualStyleBackColor = true;
+            this.btnIsExsis.Click += new System.EventHandler(this.btnIsExsis_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCreate);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 81);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(569, 63);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "创建文件夹";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(453, 20);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(84, 31);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "创建";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 25);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.btnMoveTo);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(13, 150);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(569, 63);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "移动文件夹";
+            // 
+            // btnMoveTo
+            // 
+            this.btnMoveTo.Location = new System.Drawing.Point(238, 22);
+            this.btnMoveTo.Name = "btnMoveTo";
+            this.btnMoveTo.Size = new System.Drawing.Size(94, 31);
+            this.btnMoveTo.TabIndex = 1;
+            this.btnMoveTo.Text = "移动至";
+            this.btnMoveTo.UseVisualStyleBackColor = true;
+            this.btnMoveTo.Click += new System.EventHandler(this.btnMoveTo_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(225, 25);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(338, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(225, 25);
+            this.textBox4.TabIndex = 2;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Controls.Add(this.textBox5);
+            this.groupBox4.Location = new System.Drawing.Point(13, 219);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(569, 63);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "删除文件夹";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "删除";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(7, 25);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(430, 25);
+            this.textBox5.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 296);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "文件夹的基本操作";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIsExsis;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnMoveTo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ System.IO_case/Direction_BasicOperation/Direction_BasicOperation/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Direction_BasicOperation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIsExsis_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("该文件夹已经存在");
+            }
+            else
+            {
+                MessageBox.Show("不存在该文件夹");
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox2.Text))
+            {
+                MessageBox.Show("该文件夹已经存在");
+            }
+            else
+            {
+                if (textBox2.Text == string.Empty)
+                {
+                    MessageBox.Show("路径不能为空");
+                }
+                else
+                {
+                    Directory.CreateDirectory(textBox2.Text);
+                    MessageBox.Show(textBox2.Text + "已创建");
+                }
+            }
+        }
+
+        private void btnMoveTo_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != string.Empty && textBox4.Text != string.Empty)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox3.Text);
+                directoryInfo.MoveTo(textBox4.Text);
+                MessageBox.Show("移动成功");
+            }
+            else
+            {
+                MessageBox.Show("路径不可为空");
+            }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox5.Text))
+            {
+                Directory.Delete(textBox5.Text);
+                MessageBox.Show(textBox5.Text + "已删除");
+            }
+            else
+            {
+                MessageBox.Show("不存在该文件夹");
+            }
+        }
+    }
+}
