f6f1d573c76015dfece1a5106c1aef0a74610b16
CalacalaBoom <zhangyu19990502@163.com>
2021-12-02 09:20:00 +0800

    二进制文件的读取和写入

---
+++ System.IO_case/ReadAndWriteOfbinaryFile/ReadAndWriteOfbinaryFile/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ReadAndWriteOfbinaryFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(714, 270);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(13, 312);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 37);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "读取";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(95, 312);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 37);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "写入";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 378);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "二进制文件的读取与写入";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ System.IO_case/ReadAndWriteOfbinaryFile/ReadAndWriteOfbinaryFile/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReadAndWriteOfbinaryFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "二进制文件(*.dat)|*.dat";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = string.Empty;
+                FileStream filestream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader myRead = new BinaryReader(filestream);
+                if (myRead.PeekChar() != -1)
+                {
+                    textBox1.Text = Convert.ToString(myRead.ReadInt32());
+                }
+                myRead.Close();
+                filestream.Close();
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("要写入的内容不能为空");
+            }
+            else
+            {
+                saveFileDialog1.Filter = "二进制文件(*.dat)|*.dat";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream filestream = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    BinaryWriter binarywriter = new BinaryWriter(filestream);
+                    binarywriter.Write(textBox1.Text);
+                    binarywriter.Close();
+                    filestream.Close();
+                    textBox1.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
