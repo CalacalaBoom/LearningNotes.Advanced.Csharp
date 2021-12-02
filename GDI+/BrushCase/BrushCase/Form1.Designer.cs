8cfa7b0beaf7c4aaf81a48470f808860b2dbc723
CalacalaBoom <zhangyu19990502@163.com>
2021-12-02 18:44:47 +0800

    Brush对象_填充几何图形

---
+++ GDI+/BrushCase/BrushCase/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BrushCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnPointBrush = new System.Windows.Forms.Button();
+            this.btnLG = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(12, 397);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(121, 32);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "绘制红色矩形";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnPointBrush
+            // 
+            this.btnPointBrush.Location = new System.Drawing.Point(165, 397);
+            this.btnPointBrush.Name = "btnPointBrush";
+            this.btnPointBrush.Size = new System.Drawing.Size(121, 32);
+            this.btnPointBrush.TabIndex = 1;
+            this.btnPointBrush.Text = "绘制长条图示";
+            this.btnPointBrush.UseVisualStyleBackColor = true;
+            this.btnPointBrush.Click += new System.EventHandler(this.btnPointBrush_Click);
+            // 
+            // btnLG
+            // 
+            this.btnLG.Location = new System.Drawing.Point(311, 397);
+            this.btnLG.Name = "btnLG";
+            this.btnLG.Size = new System.Drawing.Size(121, 32);
+            this.btnLG.TabIndex = 2;
+            this.btnLG.Text = "绘制渐变";
+            this.btnLG.UseVisualStyleBackColor = true;
+            this.btnLG.Click += new System.EventHandler(this.btnLG_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 441);
+            this.Controls.Add(this.btnLG);
+            this.Controls.Add(this.btnPointBrush);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "创建Brush对象";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnPointBrush;
+        private System.Windows.Forms.Button btnLG;
+    }
+}
+
---
+++ GDI+/BrushCase/BrushCase/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace BrushCase
+{
+    public partial class Form1 : Form
+    {
+        static bool isExsis=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            Graphics ghs = this.CreateGraphics();
+            if (isExsis)
+            {
+                ghs.Clear(Color.White);
+            }
+            Brush mybs = new SolidBrush(Color.Red);
+            Rectangle rt = new Rectangle(210, 100, 100, 100);
+            ghs.FillRectangle(mybs, rt);
+            isExsis = true;
+            ghs.Dispose();
+        }
+
+        private void btnPointBrush_Click(object sender, EventArgs e)
+        {
+            Graphics ghs = this.CreateGraphics();
+            if (isExsis)
+            {
+                ghs.Clear(Color.White);
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                HatchStyle hs = (HatchStyle)(5 + i);
+                HatchBrush hb = new HatchBrush(hs, Color.White);
+                Rectangle rtl = new Rectangle(10, 50 * i, 50 * i, 50);
+                ghs.FillRectangle(hb, rtl);
+            }
+            isExsis = true;
+            ghs.Dispose();
+        }
+
+        private void btnLG_Click(object sender, EventArgs e)
+        {
+            Graphics ghs = this.CreateGraphics();
+            if (isExsis)
+            {
+                ghs.Clear(Color.White);
+            }
+            Point p1 = new Point(100, 100);
+            Point p2 = new Point(150, 150);
+            LinearGradientBrush LGB = new LinearGradientBrush(p1, p2, Color.Yellow, Color.Orange);
+            LGB.WrapMode = WrapMode.Tile;
+            Rectangle rectangle = new Rectangle(210, 100, 100, 100);
+            ghs.FillRectangle(LGB, rectangle);
+            isExsis = true;
+            ghs.Dispose();
+        }
+    }
+}
