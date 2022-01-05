3094196e06ccec2fc3d374a797576c2b865fb632
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 10:10:04 +0800

    创建线程

---
+++ Multithreading/CreateThreading/CreateThreading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CreateThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mythread = new Thread(new ThreadStart(CreateThread));
+            mythread.Start();
+            Console.ReadLine();
+        }
+        public static void CreateThread()
+        {
+            Console.Write("创建一个线程");
+        }
+    }
+}
