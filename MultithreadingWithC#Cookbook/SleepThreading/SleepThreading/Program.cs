9b5be4f752e84071e00483c08002e445f749db73
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 12:58:30 +0800

    暂停线程

---
+++ MultithreadingWithC#Cookbook/SleepThreading/SleepThreading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace SleepThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintNumbersWithDelay);
+            thread.Start();
+            PrintNumbers();
+            ReadLine();
+        }
+        static void PrintNumbers()
+        {
+            WriteLine("Starting......");
+            for (int i = 0; i < 10; i++)
+            {
+                WriteLine(i);
+            }
+        }
+        static void PrintNumbersWithDelay()
+        {
+            WriteLine("Starting......");
+            for (int i = 11; i < 20; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                WriteLine(i);
+            }
+        }
+    }
+}
