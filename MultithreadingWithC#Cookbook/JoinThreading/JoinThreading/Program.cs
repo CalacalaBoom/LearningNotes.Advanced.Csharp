ff5ddb82404bf9fa02419308b2b1cf670b4d23c0
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 13:05:06 +0800

    线程等待

---
+++ MultithreadingWithC#Cookbook/JoinThreading/JoinThreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace JoinThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintNumbersWithDelay);
+            thread.Start();
+            thread.Join();
+            WriteLine("Thread completed");
+            ReadLine();
+        }
+        static void PrintNumbersWithDelay()
+        {
+            WriteLine("Starting......");
+            for (int i = 0; i < 10; i++)
+            {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine(i);
+            }
+        }
+    }
+}
