2524ffb702a264ff3f6e82ed79f0d13d325185e4
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 11:05:56 +0800

    线程优先级

---
+++ Multithreading/ThreadingPriority/ThreadingPriority/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingPriority
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mythread1 = new Thread(new ThreadStart(createThread1));
+            mythread1.Priority = ThreadPriority.Lowest;
+            Thread mythread2 = new Thread(new ThreadStart(createThread2));
+            mythread2.Priority = ThreadPriority.Highest;
+            mythread1.Start();
+            mythread2.Start();
+            Console.Read();
+        }
+        public static void createThread1()
+        {
+            Console.WriteLine("线程一执行");
+        }
+        public static void createThread2()
+        {
+            Console.WriteLine("线程二执行");
+        }
+    }
+}
