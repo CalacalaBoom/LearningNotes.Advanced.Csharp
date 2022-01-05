95e7ed63e9c2d871267c755335d38b184264d054
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 12:49:41 +0800

    创建线程

---
+++ MultithreadingWithC#Cookbook/CreateThreading/CreateThreading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace CreateThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(PrintNumbers));
+            thread.Start();
+            PrintNumbers();
+            ReadLine();
+        }
+        static void PrintNumbers()
+        {
+            WriteLine("Starting......");
+            for (int i = 0; i < 10; i++)
+            {
+                WriteLine(i);
+            }
+        }
+    }
+}
