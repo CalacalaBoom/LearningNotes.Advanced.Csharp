1549038bccf73770412c716098c12b084a71c245
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 16:10:41 +0800

    使用moniter类防止死锁

--- MultithreadingWithC#Cookbook/LockResourcesByMonitor/LockResourcesByMonitor/Program.cs
+++ MultithreadingWithC#Cookbook/LockResourcesByMonitor/LockResourcesByMonitor/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using static System.Threading.Thread;
+using static System.Console;
 
 namespace LockResourcesByMonitor
 {
@@ -10,6 +13,46 @@ namespace LockResourcesByMonitor
     {
         static void Main(string[] args)
         {
+            object lock1 = new object();
+            object lock2 = new object();
+
+            new Thread(() => LockTooMuch(lock1, lock2)).Start();
+
+            lock (lock2)
+            {
+                Sleep(1000);
+                WriteLine("Moniter.TryEnter allows not to get stuck,returning false after a specified timeout is elapsed");
+                if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(5)))
+                {
+                    WriteLine("Acquired a protected resource successfully");
+                }
+                else
+                {
+                    WriteLine("Timeout acquiring a resource");
+                }
+            }
+
+            new Thread(() => LockTooMuch(lock1, lock2)).Start();
+
+            WriteLine("--------------------");
+            lock (lock2)
+            {
+                WriteLine("This will be a deadlock!");
+                Sleep(1000);
+                lock (lock1)
+                {
+                    WriteLine("Acquired a protected resource succefully");
+                }
+            }
+            //Read()
+        }
+        static void LockTooMuch(object lock1, object lock2)
+        {
+            lock (lock1)
+            {
+                Sleep(1000);
+                lock (lock2) ;
+            }
         }
     }
 }
