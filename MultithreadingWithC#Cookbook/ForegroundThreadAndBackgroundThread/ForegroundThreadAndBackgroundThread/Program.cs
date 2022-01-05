828087ba7bf24091a1d1f8b1190808ac4961308d
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 14:36:12 +0800

    线程状态、前台线程与后台线程

---
+++ MultithreadingWithC#Cookbook/ForegroundThreadAndBackgroundThread/ForegroundThreadAndBackgroundThread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace ForegroundThreadAndBackgroundThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleForeground = new ThreadSample(10);
+            var sampleBackground = new ThreadSample(90);
+
+            var thread1 = new Thread(sampleForeground.CountNumbers);
+            thread1.Name = "ForegroundThread";
+            var thread2 = new Thread(sampleBackground.CountNumbers);
+            thread2.Name = "BackgroundThread";
+            thread2.IsBackground = true;
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+    class ThreadSample
+    {
+        private readonly int _iterations;
+        public ThreadSample(int iterations)
+        {
+            _iterations = iterations;
+        }
+        public void CountNumbers()
+        {
+            for (int i = 0; i < _iterations; i++)
+            {
+                Sleep(TimeSpan.FromSeconds(0.5));
+                WriteLine($"{CurrentThread.Name} prints {i}");
+            }
+        }
+
+    }
+}
---
+++ MultithreadingWithC#Cookbook/PriorityThread/PriorityThread/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+using static System.Diagnostics.Process;
+
+namespace PriorityThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Current thread priority:{Thread.CurrentThread.Priority}");
+            WriteLine("Running on all cores avaliable");
+            RunThread();
+            Sleep(TimeSpan.FromSeconds(2));
+            WriteLine("Running on a singgle core");
+            GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+            RunThread();
+            ReadLine();
+        }
+        static void RunThread()
+        {
+            var sample = new ThreadSample();
+            var thread1 = new Thread(sample.CountNumbers);
+            thread1.Name = "Thread1";
+            var thread2 = new Thread(sample.CountNumbers);
+            thread2.Name = "Thread2";
+
+            thread1.Priority = ThreadPriority.Highest;
+            thread2.Priority = ThreadPriority.Lowest;
+            thread1.Start();
+            thread2.Start();
+            Sleep(TimeSpan.FromSeconds(2));
+            sample.Stop();
+        }
+    }
+    class ThreadSample
+    {
+        private bool _isStoped=false;
+        public void Stop()
+        {
+            _isStoped = true;
+        }
+        public void CountNumbers()
+        {
+            long counter = 0;
+            while (!_isStoped)
+            {
+                counter++;
+            }
+            WriteLine($"{CurrentThread.Name} with {CurrentThread.Priority,11} priority,has a count = {counter}");
+        }
+    }
+}
