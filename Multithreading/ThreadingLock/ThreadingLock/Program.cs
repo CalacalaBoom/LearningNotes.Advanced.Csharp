7429f39140b384061721c493dde84541fe9d6fae
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 11:14:33 +0800

    使用lock关键字实现线程同步

---
+++ Multithreading/ThreadingLock/ThreadingLock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprograme = new Program();
+            myprograme.LockThread();
+            Console.Read();
+        }
+        void LockThread()
+        {
+            lock (this)
+            {
+                Console.WriteLine("锁定线程以实现线程同步");
+            }
+        }
+    }
+}
