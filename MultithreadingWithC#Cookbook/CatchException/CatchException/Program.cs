aad9d98e96e20e501b325a3f081afdba29c36d1d
CalacalaBoom <zhangyu19990502@163.com>
2022-01-05 16:30:29 +0800

    捕获异常

---
+++ MultithreadingWithC#Cookbook/CatchException/CatchException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Threading.Thread;
+using static System.Console;
+
+namespace CatchException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(FaultyThread);
+            t.Start();
+            t.Join();
+            try
+            {
+                t = new Thread(BadFaultyThread);
+                t.Start();
+            }
+            catch (Exception ex)
+            {
+                WriteLine("We won't get here!");
+            }
+            Read();
+        }
+        static void BadFaultyThread()
+        {
+            WriteLine("Starting a faulty thread...");
+            Sleep(TimeSpan.FromSeconds(2));
+            throw new Exception("Boom");
+        }
+        static void FaultyThread()
+        {
+            try
+            {
+                WriteLine("Starting a faulty thread...");
+                Sleep(TimeSpan.FromSeconds(1));
+                throw new Exception("Boom");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Exception handle:{ex.Message}");
+            }
+        }
+    }
+}
