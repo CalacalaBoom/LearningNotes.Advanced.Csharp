3a668b1d3c07fe51c1cc963dc8d810f78ac2c2d9
CalacalaBoom <zhangyu19990502@163.com>
2022-01-06 11:10:52 +0800

    使用SemaphoreSlim类

---
+++ MultithreadingWithC#Cookbook/ThreadSynchronization/SemaphoreSlimeEx/SemaphoreSlimeEx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace SemaphoreSlimeEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                string threadname = "Thread" + i;
+                int secondsToWait = 2 + 2 * i;
+                new Thread(() => AccessDatabase(threadname, secondsToWait)).Start();
+            }
+            
+        }
+        static SemaphoreSlim _semaphore = new SemaphoreSlim(4);
+        static void AccessDatabase(string name, int second)
+        {
+            WriteLine($"{name} waits to access a database");
+            _semaphore.Wait();
+            WriteLine($"{name} was granted an acess to a database");
+            Thread.Sleep(TimeSpan.FromSeconds(second));
+            WriteLine($"{name} is completed");
+            _semaphore.Release();
+        }
+    }
+}
