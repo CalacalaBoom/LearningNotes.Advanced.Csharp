8ecd9ce7b3b0b509eb132c204f73eb4d77c0ef4c
CalacalaBoom <zhangyu19990502@163.com>
2022-01-06 10:45:20 +0800

    使用Mutex类

---
+++ MultithreadingWithC#Cookbook/ThreadSynchronization/MutexEx/MutexEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+
+namespace MutexEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MutexName = "CSharpThreadingCookbook";
+            using (var m = new Mutex(false, MutexName))
+            {
+                if (!m.WaitOne(TimeSpan.FromSeconds(5), false))
+                {
+                    WriteLine("Second instance is running!");
+                }
+                else
+                {
+                    WriteLine("Running!");
+                    Read();
+                    m.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
