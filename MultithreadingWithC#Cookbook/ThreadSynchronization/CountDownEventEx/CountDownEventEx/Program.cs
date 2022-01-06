36a7d9fb2a07e94a8f739682cd4514a5ca447bb3
CalacalaBoom <zhangyu19990502@163.com>
2022-01-06 13:02:54 +0800

    CountDownEvent类的使用

---
+++ MultithreadingWithC#Cookbook/ThreadSynchronization/CountDownEventEx/CountDownEventEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace CountDownEventEx
+{
+    class Program
+    {
+        static CountdownEvent _countdown = new CountdownEvent(2);
+        static void Main(string[] args)
+        {
+            WriteLine("Starting two opreations");
+            new Thread(() => PerformOperation("Operation 1 is completed", 4)).Start();
+            new Thread(() => PerformOperation("Operation 2 is completed", 8)).Start();
+            _countdown.Wait();
+            WriteLine("Both operatins have been completed.");
+            _countdown.Dispose();
+            ReadLine();
+        }
+        static void PerformOperation(string message, int seconds)
+        {
+            Sleep(TimeSpan.FromSeconds(seconds));
+            WriteLine(message);
+            _countdown.Signal();
+        }
+        
+    }
+}
